bf5cfb4eaa394b52fae06ad57df92cb0d493e5f7
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-11-09 14:56:45 -0500

    Basic AspNetCore Template with Vite/Vue

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Smilodon
+
+## Running Development Build Locally
+
+### Running the WebApp (API) locally
+
+To run the WebApp, you can navigate to the `\WebApp` folder from your preferred commmand line and run the following command:
+
+``` bat
+dotnet run watch
+```
+
+If you want the combined experience of both sites running together, the AspNetCore WebApp project is set up to proxy all non-handled requests to the "http://localhost:1336", which is the address of the Client app if you run the client site locally.
+
+### Running the ClientApp (Front-End) locally
+
+The client application can be run from the command line using the following command while in the `\WebApp\ClientApp` folder:
+
+``` bat
+npm run dev
+```
---
+++ src/WebApp/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(x => 
+{
+    x.MapControllerRoute("default", "{controller}/{action}/{id?}");
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSpa(x => 
+    {
+        x.UseProxyToSpaDevelopmentServer("http://localhost:1336");
+    });
+}
+else
+{
+app.MapFallbackToFile("index.html");
+}
+
+app.Run();

e42eab8724892afb1f3fe7cda61e11da99937a96
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-11-21 18:56:09 -0500

    Treat Warnings as Errors (#12)
    * Fix warnings
    
    * Treat warnings as errors

--- src/WebApp/Models/Webfinger.cs
+++ src/WebApp/Models/Webfinger.cs
@@ -1,16 +1,6 @@
 namespace Smilodon.WebApp.Models;
 
 // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-public record Link
-{
-    public string rel { get; set; }
-    public string type { get; set; }
-    public string href { get; set; }
-}
+public record Link(string rel, string type, string href);
 
-public record WebFinger
-{
-    public string subject { get; set; }
-    public List<Link> links { get; set; }
-}
-
+public record WebFinger(string subject, List<Link> links);

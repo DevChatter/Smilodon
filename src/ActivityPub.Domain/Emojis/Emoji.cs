5efcb25e80c508baca7a048d83aa3e047f107a01
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-11-22 11:02:14 -0500

    Add emoji comments

--- src/ActivityPub.Domain/Emojis/Emoji.cs
+++ src/ActivityPub.Domain/Emojis/Emoji.cs
@@ -2,8 +2,28 @@
 
 public class Emoji
 {
+    /// <summary>
+    /// The name of the custom Emoji.
+    /// </summary>
     public string ShortCode { get; set; }
+
+    /// <summary>
+    /// URL to the custom Emoji image.
+    /// </summary>
     public string Url { get; set; }
+
+    /// <summary>
+    /// URL to the static version of the custom Emoji.
+    /// </summary>
     public string StaticUrl { get; set; }
-    public bool VisibleInPicker { get; set; }
+
+    /// <summary>
+    /// Whether this Emoji should be visible in the picker or unlisted.
+    /// </summary>
+    public bool IsVisibleInPicker { get; set; }
+
+    /// <summary>
+    /// Used for grouping custom emojis.
+    /// </summary>
+    public string Category { get; set; }
 }

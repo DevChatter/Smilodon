e7ba45b6e9f7fe5d96c642bb50d5da917c682a8e
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-11-22 10:53:15 -0500

    Sample Model Objects

---
+++ src/ActivityPub.Domain/Accounts/ProfileField.cs
@@ -0,0 +1,21 @@
+﻿namespace ActivityPub.Domain.Accounts
+{
+    public class ProfileField
+    {
+        /// <summary>
+        /// The key of a given field’s key-value pair.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The value associated with the name key. Can have HTML.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Timestamp of when the server verified a URL value for a rel=“me” link.
+        /// Null means not verified.
+        /// </summary>
+        public DateTime? VerifiedAt { get; set; }
+    }
+}
---
+++ src/ActivityPub.Domain/Emojis/Emoji.cs
@@ -0,0 +1,9 @@
+﻿namespace ActivityPub.Domain.Emojis;
+
+public class Emoji
+{
+    public string ShortCode { get; set; }
+    public string Url { get; set; }
+    public string StaticUrl { get; set; }
+    public bool VisibleInPicker { get; set; }
+}

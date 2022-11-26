d5c1977f8adfe5a42327d735f698b8344dddfb20
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-11-26 00:14:47 -0500

    AddDbContext before builder.Build()

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -9,6 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+// Add the database context to the DI container
+builder.Services.AddDbContext<SmilodonDbContext>(options =>
+    options
+        .UseNpgsql("Host=localhost; Database=smilodon; User Id=smilodon; Password=smilodon;")
+        .UseSnakeCaseNamingConvention());
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,12 +24,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-// Add the database context to the DI container
-builder.Services.AddDbContext<SmilodonDbContext>(options =>
-    options
-        .UseNpgsql("Host=localhost;Database=smilodon;User Id=smilodon;Password=smilodon;")
-        .UseSnakeCaseNamingConvention());
 
 app.UseHttpsRedirection();
 

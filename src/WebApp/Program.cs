da7a46805cc0e8cd098b77dca8c6e4ae2b84bf84
edmistond <edmistond@gmail.com>
2022-11-27 22:44:57 -0500

    add basic opentelemetry implementation

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,11 +1,35 @@
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Smilodon.Infrastructure.Persistence;
 using Smilodon.WebApp.Api.Admin;
 using Smilodon.WebApp.Api.Webfinger;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// this should really be in the app config instead...
+const string serviceName = "Smilodon.WebApp";
+const string serviceVersion = "0.0.1";
+
 // Add services to the container.
+
+builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
+{
+    tracerProviderBuilder
+        .AddConsoleExporter()
+        .AddOtlpExporter(opt =>
+        {
+            opt.Protocol = OtlpExportProtocol.HttpProtobuf;
+        })
+        .AddSource(serviceName)
+        .SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddSqlClientInstrumentation();
+});
+
 
 builder.Services.AddControllersWithViews();
 
